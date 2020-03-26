de35e737521654b64f4086b2d35d370a6dead297
Ariesfish <zzlyldy@gmail.com>
2020-03-26 17:54:18 +0900

    add hystrix feign client

---
+++ cloud-customer-feign-hystrix-order-80/src/main/java/xyz/ariesfish/springcloud/controller/OrderHystrixController.java
@@ -0,0 +1,61 @@
+package xyz.ariesfish.springcloud.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.ariesfish.springcloud.service.PaymentHystrixService;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
+public class OrderHystrixController {
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
+
+    /**
+     * 正常访问: http://localhost/consumer/payment/hystrix/ok/1
+     * @param id
+     * @return
+     */
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
+        return paymentHystrixService.paymentInfo_OK(id);
+    }
+
+    /**
+     * 超时访问: http://localhost/consumer/payment/hystrix/timeout/1
+     * @param id
+     * @return
+     */
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    /*@HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+    })*/
+    @HystrixCommand // 只有注解时使用默认的fallback
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        int age = 10 / 0;
+        return paymentHystrixService.paymentInfo_TimeOut(id);
+    }
+
+    /**
+     * 超时访问的fallback
+     * @param id
+     * @return
+     */
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
+        return "这是消费者80, 对方支付系统繁忙请10秒种后再试或者运行出错请检查";
+    }
+
+    /**
+     * 全局的fallback
+     * @return
+     */
+    public String payment_Global_FallbackMethod() {
+        return "Global异常处理信息, 请稍后重试";
+    }
+}
